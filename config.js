f438b518aee13e09e1c44b44202b71116da45d70
aglahir <aglahirjf@gmail.com>
2020-05-28 16:41:20 -0500

    Fixed FS routes

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 module.exports = {
   DATABASE_URL:
     "mongodb+srv://admin:admin@webclass-qgmrz.mongodb.net/finalweb?retryWrites=true&w=majority",
-  PORT: 8080,
+  PORT: 80,
   SECRET_TOKEN: process.env.SECRET_TOKEN || "1234",
 };
